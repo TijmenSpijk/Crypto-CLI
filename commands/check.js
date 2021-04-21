b13fa02203243838de245eeb15b1b5c08adbab2b
Tijmen van der Spijk <tijmenspijk@gmail.com>
2021-04-21 21:25:02 +0200

    added check for the interval option

--- commands/check.js
+++ commands/check.js
@@ -11,40 +11,63 @@ const check = {
             const key = keyManager.getKey();
 
             const api = new CryptoAPI(key);
-            const res = await api.getPriceData(cmd.coin, cmd.cur, cmd.int);
-            const output = this.formatData(res, cmd.int, cmd.cur);
+            var intervals = checkInterval(cmd.int);
+            const res = await api.getPriceData(cmd.coin, cmd.cur, intervals);
+            const output = formatData(res, intervals, cmd.cur);
 
             console.log(output);
         } catch (error) {
-            console.error(error.red);
+            handleAPIError(error);
         }
-    },
-    formatData(data, intervals, currency) {
-        const interval = intervals.split(',')
-        var output = '';
+    }
+}
 
-        const formatter = new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: currency
-        })
+function checkInterval(intervals) {
+    const interval = intervals.split(',');
+    var valid = ''
+    interval.forEach(int => {
+        if (['1d','7d','30d','365d'].includes(int)) {
+            valid += int + ','
+        }
+    });
+    return valid.substring(0, valid.length-1);
+}
 
-        data.forEach(coin => {
-            output += `Coin: ${coin.symbol.yellow} (${coin.name}) | Price: ${formatter.format(coin.price).green} | High: ${formatter.format(coin.high).blue} `;
-            
-            interval.forEach(int => {
-                let priceChange = coin[int].price_change;
-                priceChange = formatter.format(priceChange);
-                priceChange = priceChange[0] == '-' ? priceChange.red : priceChange.green;
+function formatData(data, intervals, currency) {
+    const interval = intervals.split(',')
+    var output = '';
 
-                let priceChangePTC = coin[int].price_change_pct;
-                priceChangePTC = priceChangePTC[0] == '-' ? priceChangePTC.red : priceChangePTC.green;
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: currency
+    })
 
-                output += `| Change (${int}) ${priceChange} (${priceChangePTC})`
-            });
-            
-            output += '\n'
+    data.forEach(coin => {
+        output += `Coin: ${coin.symbol.yellow} (${coin.name}) | Price: ${formatter.format(coin.price).green} | High: ${formatter.format(coin.high).blue} `;
+        
+        interval.forEach(int => {
+            let priceChange = coin[int].price_change;
+            priceChange = formatter.format(priceChange);
+            priceChange = priceChange[0] == '-' ? priceChange.red : priceChange.green;
+
+            let priceChangePTC = coin[int].price_change_pct;
+            priceChangePTC = priceChangePTC[0] == '-' ? priceChangePTC.red : priceChangePTC.green;
+
+            output += `| Change (${int}) ${priceChange} (${priceChangePTC})`
         });
-        return output;
+        
+        output += '\n'
+    });
+    return output;
+}
+
+function handleAPIError(error) {
+    if (error.respondse.status === 401) {
+        throw new Error('Invalid API Key -- Go to https://nomics.com');
+    } else if (error.respondse.status === 404) {
+        throw new Error('API is not responding -- Go to https://nomics.com');
+    } else {
+        throw new Error('Oops something went wrong');
     }
 }
 
